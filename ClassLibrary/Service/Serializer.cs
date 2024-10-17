268d053ddc3a032f3c81c8068bc4615aa0cd1ab6
BhaskariShrikanth <BhaskariShrikanth@MONO-77D35L2>
2024-10-17 23:39:46 +0530

    Add project files.

---
+++ ClassLibrary/Service/Serializer.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Service
+{
+    public class Serializer
+    {
+        static string filepath = "Shrikanth.json";
+        public static void Serialization(List<Guitar> guitars)
+        {
+            var json = JsonSerializer.Serialize(guitars);
+            File.WriteAllText(filepath, json);
+
+        }
+        public static List<Guitar> Deserialize()
+        {
+            if (!File.Exists(filepath))
+            {
+                return new List<Guitar>();
+            }
+            var json = File.ReadAllText(filepath);
+            return JsonSerializer.Deserialize<List<Guitar>>(json);
+        }
+    }
+}
+
---
+++ GuitarMain/GuitarMain/Program.cs
@@ -0,0 +1,12 @@
+﻿using GuitarMain.Presentation;
+
+namespace GuitarMain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GuitarMenu.Menu();
+        }
+    }
+}
